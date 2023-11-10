7296d225e32677edb2be6a698ab6a81aaa3cd0fc
Titanovsky <titanovskyteam@gmail.com>
2023-11-10 19:20:22 +0500

    minor changes and optimization for old module

--- ambi/lua/modules/player-freeze/core/sv_player_freeze.lua
+++ ambi/lua/modules/player-freeze/core/sv_player_freeze.lua
@@ -3,11 +3,11 @@ local PLAYER = FindMetaTable( 'Player' )
 function PLAYER:FreezeMove( bFreeze, nTime )
     self.frozen = bFreeze
 
-    if nTime and isnumber( nTime ) and ( nTime > 0 ) and ( nTime < 268435456 ) then
-        timer.Simple( nTime, function()
-            if IsValid( self ) then self.frozen = false end
-        end )
-    end
+    if not nTime then return end
+
+    timer.Simple( nTime, function()
+        if IsValid( self ) then self.frozen = false end
+    end )
 end
 
 function PLAYER:IsFrozenMove()
@@ -15,11 +15,10 @@ function PLAYER:IsFrozenMove()
 end
 
 hook.Add( 'SetupMove', 'Ambi.PlayerFreeze.StopMove', function( ePly, cmoveInfo, cuserCommand )
-    if ( Ambi.PlayerFreeze.enable == false ) then return end
+    if not Ambi.PlayerFreeze.enable then return end
+    if not ePly:IsFrozenMove() then return end
 
-    if ePly.frozen and ( cmoveInfo:KeyDown( IN_FORWARD ) or cmoveInfo:KeyDown( IN_MOVELEFT ) or cmoveInfo:KeyDown( IN_MOVERIGHT ) or cmoveInfo:KeyDown( IN_BACK ) ) then  
-        cmoveInfo:SetForwardSpeed( 0.00 )
-        cmoveInfo:SetSideSpeed( 0.00 )
-        cmoveInfo:SetUpSpeed( 0.00 )
-    end
+    cmoveInfo:SetForwardSpeed( 0.00 )
+    cmoveInfo:SetSideSpeed( 0.00 )
+    cmoveInfo:SetUpSpeed( 0.00 )
 end )
